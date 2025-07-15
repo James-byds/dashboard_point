8dac980989b0bc22b3981ab39c15d405638607f2
NEPotato <james.boydens@gmail.com>
2025-07-15 13:23:36 +0200

    Display_formations

---
+++ formation.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'functions.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formations</title>
+</head>
+<body>
+  <?php
+  include 'nav.php';
+  ?>
+  <h1>Formations</h1>
+  <p>This is the formation page.</p>
+  <?php
+    $params = [
+      'fields' => json_encode([
+          '_modified' => 0,
+          '_mby' => 0,
+          '_created' => 0,
+          '_state' => 0,
+          '_cby' => 0,
+          '_id' => 0
+      ])
+      ];
+    model_display($params, "formations");
+  ?>
+
+</body>
+</html>
--- nav.php
+++ nav.php
@@ -2,6 +2,6 @@
   <ul>
     <li><a href="./dashboard.php">Home</a></li>
     <li><a href="./staff.php">Personnel</a></li>
-    <li><a href="./formations.php">Formations</a></li>
+    <li><a href="./formation.php">Formations</a></li>
   </ul>
 </nav>
