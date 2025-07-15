1b640239f6ea2dda79998868dd7c19f470573248
NEPotato <james.boydens@gmail.com>
2025-07-15 12:55:09 +0200

    add_curl

---
+++ remove.php
@@ -0,0 +1,21 @@
+<?php 
+//remove entry from the api
+$id = parseInt($_GET['id']);
+$model= trim($_GET['model']);
+//curl parameters
+$params = [
+  'filter' => json_encode([
+    'id' => $id
+])
+];
+
+//curl request
+$url = curl_init(apiUrlSolo . '/'.$model.'?'.http_build_query($params)); // API endpoint for fetching history items
+curl_setopt($url, CURLOPT_RETURNTRANSFER, true); // Return the response as a string
+curl_setopt($url, CURLOPT_CUSTOMREQUEST, 'DELETE');
+curl_setopt($url, CURLOPT_HTTPHEADER, [
+  'Content-Type: application/json',
+  'Accept: application/json'
+]);
+$response = curl_exec($url); // Execute the request
+curl_close($url); // Close the cURL session
